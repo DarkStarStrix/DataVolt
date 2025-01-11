f3d2191e193a403f0dab057603c34f9022dd1829
Darkstalker <allanw.mk@gmail.com>
2025-01-11 18:16:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
+import os
 from setuptools import setup, find_packages
 
+# Check if README.md exists
+readme = "README.md"
+if os.path.exists(readme):
+    with open(readme, "r") as fh:
+        long_description = fh.read()
+else:
+    long_description = ""
+
 setup(
-    name='datastream',
-    version='0.0.1',
-    author='Allan',
-    author_email='allaw.mk@gmail.com',
-    description='A modular toolkit for data engineering pipelines',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/dataflux',
+    name="DataVolt",
+    version="0.0.1",
+    author="Allan",
+    author_email="allanw.mk@gmail.com",
+    description="A resuable workflow for data engineering piplines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DarkStarStrix/DataVolt",
     packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3.9+',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
-    install_requires=[
-        'pandas>=1.3.0',
-        "numpy==1.22.0",
-        "scikit-learn==1.5.0",
-        'neptune-client>=0.9.0',
-        "torch==2.5.0",
-        'setuptools>=70.0.0',
-        'pillow~=11.0.0',
-        'joblib~=1.4.2',
-        'boto3~=1.35.88',
-        'sqlalchemy~=1.4.0'
-    ],
+    python_requires='>=3.9+',
 )
